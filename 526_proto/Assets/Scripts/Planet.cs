57a1b726dd383f5a18c0a81be313b45f1621ab3a
franwrockwell <33009525+franwrockwell@users.noreply.github.com>
2019-02-04 16:01:12 -0800

    adding project

---
+++ 526_proto/Assets/Scripts/Planet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+    Rocket r;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        r = GameObject.FindGameObjectWithTag("Player").GetComponent<Rocket>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (r.isOn)
+        {
+            r.rb.AddForce((transform.position - r.transform.position) * 0.25f);
+        }
+    }
+}
---
+++ 526_proto/Assets/Scripts/Rocket.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+
+    public bool isOn = false;
+    public Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("w"))
+            isOn = true;
+
+
+        if (isOn)
+        {
+            rb.AddForce(Vector2.up);
+        }
+    }
+}
